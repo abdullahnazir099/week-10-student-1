ae924b01e4bcab22b1122890e5f5676a674d0f08
abdullal <abdullahnazir.099@gmail.com>
2024-05-05 23:17:19 +0500

    updated

--- contact_view.js
+++ contact_view.js
@@ -1,8 +1,8 @@
- // Extract contact ID from the URL parameters
+
  const urlParams = new URLSearchParams(window.location.search);
  const contactId = urlParams.get('id');
 
- // Fetch contact information based on the ID
+
  const fetchContact = async () => {
      try {
          const response = await fetch(`/.netlify/functions/contact_read?id=${contactId}`);
@@ -10,7 +10,7 @@
              throw new Error('Failed to fetch contact');
          }
          const data = await response.json();
-         // Update form fields with fetched contact information
+
          document.getElementById('contactId').value = data.id;
          document.getElementById('contactName').value = data.first_name + ' ' + data.last_name;
          document.getElementById('contactEmail').value = data.email;
@@ -20,22 +20,20 @@
      }
  };
 
- // Call fetchContact function when the page loads
+
  window.onload = fetchContact;
 
- // Function to handle form submission for updating contact details
- const updateContact = async (event) => {
-     event.preventDefault(); // Prevent default form submission behavior
 
-     // Get form data
+ const updateContact = async (event) => {
+     event.preventDefault();
+   
      const contactName = document.getElementById('contactName').value;
      const [firstName, lastName] = contactName.split(' ');
      const email = document.getElementById('contactEmail').value;
      const phone = document.getElementById('contactPhone').value;
 
-     // Make POST request to serverless function to update contact
      try {
-         const response = await fetch('/.netlify/functions/contact_update', {
+         const response = await fetch('/.netlify/functions/updatecontact', {
              method: 'POST',
              headers: {
                'Content-Type': 'application/json',
@@ -45,7 +43,7 @@
          
          if (response.ok) {
              alert('Contact updated successfully!');
-             // Redirect back to Contacts page after successful update
+
              window.location.href = 'contacts.html';
          } else {
              throw new Error('Failed to update contact');
@@ -56,5 +54,5 @@
      }
  };
 
- // Attach event listener to the form submit button
+
  document.getElementById('saveChangesBtn').addEventListener('click', updateContact);
