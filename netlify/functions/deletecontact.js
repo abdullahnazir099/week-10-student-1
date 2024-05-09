07259f88fbb56e599ff8fce34f48b5ea492e577f
abdullal <abdullahnazir.099@gmail.com>
2024-05-09 21:40:58 +0500

    updated

--- netlify/functions/deletecontact.js
+++ netlify/functions/deletecontact.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
       database: process.env.DB_NAME
     });
 
-    const sql = 'DELETE FROM contact WHERE id = ?';
+    const sql = 'DELETE FROM contacts WHERE id = ?';
     await connection.execute(sql, [id]);
     await connection.end();
 
